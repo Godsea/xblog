09381334f3646f52b30e16dc916c4eb751d04256
godsea <1347951306@qq.com>
2024-11-25 14:02:13 +0800

    init

--- article/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . import models
-
-admin.site.register(models.Navigation)
-admin.site.register(models.Contents)
-admin.site.register(models.Types)
-admin.site.register(models.Blogs)
--- xblog/__init__.py
+++
@@ -1,3 +0,0 @@
-#import pymysql
-#pymysql.install_as_MySQLdb()
-import MySQLdb
